0d5f089da3eb15dc68a3acc6abfa9574e2333b5f
LUNA627 <say.samurai@yandex.ru>
2025-04-02 23:08:14 +0500

    Delete Main (1).kt

--- Main (1).kt
+++
@@ -1,7 +0,0 @@
-fun main() {
-    var dayOfWeek: Week = Week.Sunday
-    println (dayOfWeek.Name())
-    println("Порядковый номер - ${dayOfWeek.number}")
-    println(dayOfWeek.Str())
-    println(dayOfWeek.Discount())
-}
