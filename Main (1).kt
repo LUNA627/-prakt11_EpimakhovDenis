2d56f3a12299ff62757471721a6aee1e7c2bdc18
LUNA627 <say.samurai@yandex.ru>
2025-04-02 15:48:40 +0500

    Add files via upload

---
+++ Main (1).kt
@@ -0,0 +1,7 @@
+fun main() {
+    var dayOfWeek: Week = Week.Sunday
+    println (dayOfWeek.Name())
+    println("Порядковый номер - ${dayOfWeek.number}")
+    println(dayOfWeek.Str())
+    println(dayOfWeek.Discount())
+}
